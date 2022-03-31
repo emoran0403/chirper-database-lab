6cfc0fc46776adb6c58392fdbbb892c31163b1a4
Eric Moran <emoran0403@gmail.com>
2022-03-31 10:47:37 -0400

    added conditional render logic to buttons and inputs

--- src/client/Inputs.tsx
+++ src/client/Inputs.tsx
@@ -12,6 +12,36 @@ const Inputs = () => {
   const [userIsUpdating, setUserIsUpdating] = useState<boolean>(false);
   const [userIsDeleting, setUserIsDeleting] = useState<boolean>(false);
 
+  // lets us conditionally render the crud buttons - when one is clicked, hide them all
+  // evaluates to true when user is doing any CRUD operation
+  // { !crudButtonClicked && ( crud button JSX here)} // when user is doing CRUD, hide the CRUD buttons
+  let crudButtonClicked: boolean = userIsCreating || userIsReading || userIsUpdating || userIsDeleting;
+
+  // lets us conditionally render the submit and cancel buttons - when a crud button is clicked, show them all
+  // submit and cancel button functionality includes resetting crud buttons to false, which shows crud buttons and subsequently hides submit / cancel
+  // { !hideSubmitCancelButtons && ( submit and cancel JSX here)} // when user is doing CRUD, show the submit and cancel buttons
+  let hidingSubmitCancelButtons: boolean = !userIsCreating || !userIsReading || !userIsUpdating || !userIsDeleting;
+
+  /******************************  Conditional render logic for input boxes  **************************/
+
+  // evaluates to true when user is reading OR deleting
+  // { !showChirpBox && (chirpBox JSX here)} // used to hide the chirpBox when user is reading or deleting
+  let showChirpBox: boolean = userIsReading || userIsDeleting;
+
+  // evaluates to true when user is creating
+  // { !showIDBox && (IDBox JSX here)} // used to hide the IDBox when the user is creating
+  let showIDBox: boolean = !userIsCreating;
+
+  // evaluates to true when user is reading OR deleting
+  // { !showLocationBox && (locationBox JSX here)} // used to hide the locationBox when user is reading or deleting
+  let showLocationBox: boolean = !userIsReading || !userIsDeleting;
+
+  /**
+   * chirp - show on create and update - hide on read and delete
+   * id - show on read and update and delete - hide on create
+   * location - show on create and update - hide on read and delete
+   */
+
   // useEffect(() => {}, [userIsCreating, userIsReading, userIsUpdating, userIsDeleting]);
   // might need this later
 
@@ -91,34 +121,58 @@ const Inputs = () => {
     <main className="container">
       <div>
         <form>
-          <input id="textBox" className="form-control mt-3" value={textBoxContent} onChange={(e) => handletextBoxContentChange(e)} placeholder="Chirp box!" type="text" />
-          <input id="IDBox" className="form-control mt-3" value={IDBoxContent} onChange={(e) => handleIDBoxContentChange(e)} placeholder="Chirp box!" type="text" />
-          <input
-            id="locationBox"
-            className="form-control mt-3"
-            value={locationBoxContent}
-            onChange={(e) => handleLocationBoxContentChange(e)}
-            placeholder="Chirp box!"
-            type="text"
-          />
-          <button id="createBtn" disabled={userIsReading || userIsUpdating || userIsDeleting} onClick={(e) => handleUserIsCreating(e)} className="btn-primary">
-            Create
-          </button>
-          <button id="readBtn" disabled={userIsCreating || userIsUpdating || userIsDeleting} onClick={(e) => handleUserIsReading(e)} className="btn-primary">
-            Read
-          </button>
-          <button id="updateBtn" disabled={userIsCreating || userIsReading || userIsDeleting} onClick={(e) => handleUserIsUpdating(e)} className="btn-primary">
-            Update
-          </button>
-          <button id="deleteBtn" disabled={userIsCreating || userIsReading || userIsUpdating} onClick={(e) => handleUserIsDeleting(e)} className="btn-primary">
-            Delete
-          </button>
-          <button id="submitBtn" onClick={(e) => handleSubmitButton(e)} className="btn-primary">
-            Submit
-          </button>
-          <button id="cancelBtn" onClick={(e) => handleCancelButton(e)} className="btn-primary">
-            Cancel
-          </button>
+          <div>Click a button to get started</div>
+
+          {/*************************************  CRUD BUTTONS  ***************************************/}
+
+          {!crudButtonClicked && (
+            <>
+              <button id="createBtn" onClick={(e) => handleUserIsCreating(e)} className="btn-primary">
+                Create
+              </button>
+              <button id="readBtn" onClick={(e) => handleUserIsReading(e)} className="btn-primary">
+                Read
+              </button>
+              <button id="updateBtn" onClick={(e) => handleUserIsUpdating(e)} className="btn-primary">
+                Update
+              </button>
+              <button id="deleteBtn" onClick={(e) => handleUserIsDeleting(e)} className="btn-primary">
+                Delete
+              </button>
+            </>
+          )}
+
+          {/*************************************  SUBMIT / CANCEL BUTTONS  ***************************************/}
+
+          {!hidingSubmitCancelButtons && (
+            <>
+              <button id="submitBtn" onClick={(e) => handleSubmitButton(e)} className="btn-primary">
+                Submit
+              </button>
+              <button id="cancelBtn" onClick={(e) => handleCancelButton(e)} className="btn-primary">
+                Cancel
+              </button>
+            </>
+          )}
+
+          {/*************************************  FORM INPUTS  ***************************************/}
+
+          {userIsReading && <div>Enter an id to view a single chirp, or leave blank to view all chirps.</div>}
+
+          {!showChirpBox && (
+            <input id="textBox" className="form-control mt-3" value={textBoxContent} onChange={(e) => handletextBoxContentChange(e)} placeholder="Chirp box!" type="text" />
+          )}
+          {!showIDBox && <input id="IDBox" className="form-control mt-3" value={IDBoxContent} onChange={(e) => handleIDBoxContentChange(e)} placeholder="Chirp box!" type="text" />}
+          {!showLocationBox && (
+            <input
+              id="locationBox"
+              className="form-control mt-3"
+              value={locationBoxContent}
+              onChange={(e) => handleLocationBoxContentChange(e)}
+              placeholder="Chirp box!"
+              type="text"
+            />
+          )}
         </form>
       </div>
     </main>
@@ -126,3 +180,15 @@ const Inputs = () => {
 };
 
 export default Inputs;
+
+/**
+ *
+ * crud buttons are in the form box by default
+ * inputs and sumbit / cancel are hidden
+ *
+ * when a crud button is clicked all crud buttons disappear, and are replaced with submit and cancel
+ * the relevant inputs for the clicked crud button appears
+ * can i make the sumbit and cancel take up the full width as the crud buttons?
+ */
+
+// { condition && (dothiswhentrue)} //shows jsx when condition is true.  if false, returns nothing
